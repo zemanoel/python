78d791c7b20854dfe998b862f50295dccb0e2f2d
Zé Manoel <50956353+zemanoel@users.noreply.github.com>
2021-11-29 14:37:26 -0300

    Update get_ip_address.py

--- get_ip_address.py
+++ get_ip_address.py
@@ -1,5 +1,8 @@
 import socket
+import os
+
 hostname = socket.gethostname()
 name = socket.gethostbyname(hostname)
 print(f'O nome do seu Computador: {hostname}')  # retorna o nome da maquina
 print(f'O IP do seu computador na rede: {name}')  # retorna o IP da maquina
+os.system('pause')
